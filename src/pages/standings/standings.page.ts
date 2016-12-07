110be14e45b1fc76805f663c4425bc62935f43f0
Peter <eskandar.peter@accenture.com>
2016-12-07 14:25:23 +0100

    new standings page

--- src/pages/standings/standings.page.ts
+++ src/pages/standings/standings.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import {EliteApi} from '../../shared/elite-api.service';
+import * as _ from 'lodash';
 
 /*
   Generated class for the Standings page.
@@ -13,10 +16,28 @@ import { NavController } from 'ionic-angular';
 })
 export class StandingsPage {
 
-  constructor(public navCtrl: NavController) {}
+  standings: any[];
+  allStandings: any;
+  team: any;
+
+  constructor(public navCtrl: NavController,
+              private navParama : NavParams,
+              private eliteApi : EliteApi) {}
 
   ionViewDidLoad() {
     console.log('Hello StandingsPage Page');
+    this.team = this.navParama.data;
+    let tourneyData = this.eliteApi.getCurrentTourney();
+    this.standings = tourneyData.standings;
+
+    this.allStandings = _.chain(this.standings)
+                        .groupBy('division')
+                        .toPairs()
+                        .map(item => _.zipObject(['divisionName','divisionStandings'], item))
+                        .value();
+    console.log('division standings ' , this.allStandings)
+
+
   }
 
 }
